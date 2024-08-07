952966e013946265c32ec9e0e5922e07a06b2a17
Konstadina Kladis <konstadinakladis@gmail.com>
2024-08-07 10:27:20 +0000

    Purchase cost and margin calculated in int sales margin model

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,7 +1,14 @@
-select
-  quantity * purchase_price as purchase_cost
+select 
+  products_id,
+  date_date,
+  orders_id,
+  revenue,
+  quantity,
+  cast(purchase_price as float64) as purchase_price,
+  round(quantity*cast(purchase_price as float64),2) as purchase_cost,
+  round(revenue - round(quantity*cast(purchase_price as float64),2),2) as margin
 from 
   {{ref('stg_raw__sales')}}
-join
+left join
   {{ref('stg_raw__product')}}
 using (products_id)
